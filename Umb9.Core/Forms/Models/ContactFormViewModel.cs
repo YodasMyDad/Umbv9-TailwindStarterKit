4d4d7093bc56329f4b815b37d0bb38c7f3ceede2
Lee Messenger <lee@aptitude.co.uk>
2021-10-04 15:27:01 +0100

    Starting work on the ContactFormBlock

---
+++ Umb9.Core/Forms/Models/ContactFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umb9.Core.Forms.Models
+{
+    public class ContactFormViewModel
+    {
+    }
+}
